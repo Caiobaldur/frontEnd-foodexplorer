255dfa760689d91fbe4fbf2198134870ffc1dc00
Caio Balduino <caiobrafachino@hotmail.com>
2024-04-18 18:48:03 -0300

    feat: mobile style & position

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,19 +5,17 @@ export function Login() {
 
   return (
     <Container>
-      <div>
-        <div>
+        <div className="logo">
           <img src="../src/assets/logo_food.svg" alt="" />
         </div>
       <Form>
-        <label htmlFor="email">E-mail</label>
-        <input placeholder="Exemplo: exemplo@exemplo.com.br" type="text" id="email"/>
+          <label htmlFor="email">Email</label>
+          <input placeholder="Exemplo: exemplo@exemplo.com.br" type="text" id="email"/>
         <label htmlFor="password">Senha</label>
         <input placeholder="No mínimo 6 caracteres" type="text" id="password"/>
-      </Form>
-
     <Button title="Entrar"/>
-      </div>
+      </Form>
+    
     </Container>
   );
 
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,22 +1,36 @@
 import styled from "styled-components";
 
-export const Container = styled.div `
-
+export const Container = styled.div`
   color: white;
+
+  @media (max-width: 500px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 15.8rem;
+    gap: 7.3rem;
+  }
 `;
 
-export const Form = styled.form `
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+export const Form = styled.form`
 
-  & input{
-    background-color: ${({theme}) => theme.COLORS.DARK_900};
-    padding: 1.6rem 1.4rem;
-    border-radius: .8rem;
-    margin-bottom: 3.2rem;
+@media (max-width: 500px) {
+    width: 31.6rem;
+    font-family: ${({ theme }) => theme.FONTS.Secondary};
+    font-size: 1.6rem;
+    display: flex;
+    flex-direction: column;
+    & label {
+      margin-bottom: 0.8rem;
+    }
+
+    & input {
+      background-color: ${({ theme }) => theme.COLORS.DARK_900};
+      width: 100%;
+      padding: 1.6rem 1.4rem;
+      border-radius: 0.8rem;
+      margin-bottom: 3.2rem;
+    }
   }
-
 `;

1ea4470ebf9b48323a4810f4ec2d8fb26b7d8835
Caio Balduino <caiobrafachino@hotmail.com>
2024-04-17 23:32:02 -0300

    feat: initial structure login & component button

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,23 @@
-import { Container } from "./styles";
-
+import { Button } from "../../components/Button/Button";
+import { Container, Form } from "./styles";
 
 export function Login() {
 
   return (
     <Container>
-      
+      <div>
+        <div>
+          <img src="../src/assets/logo_food.svg" alt="" />
+        </div>
+      <Form>
+        <label htmlFor="email">E-mail</label>
+        <input placeholder="Exemplo: exemplo@exemplo.com.br" type="text" id="email"/>
+        <label htmlFor="password">Senha</label>
+        <input placeholder="No mínimo 6 caracteres" type="text" id="password"/>
+      </Form>
+
+    <Button/>
+      </div>
     </Container>
   );
 
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -4,3 +4,19 @@ export const Container = styled.div `
 
   color: white;
 `;
+
+export const Form = styled.form `
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  & input{
+    background-color: ${({theme}) => theme.COLORS.DARK_900};
+    padding: 1.6rem 1.4rem;
+    border-radius: .8rem;
+    margin-bottom: 3.2rem;
+  }
+
+`;

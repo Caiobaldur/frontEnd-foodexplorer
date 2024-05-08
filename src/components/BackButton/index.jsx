dc5c668027e0a3ef05c3e079e326b5889e348427
Caio Balduino <caiobrafachino@hotmail.com>
2024-05-07 22:09:27 -0300

    feat: back button action click

--- src/components/BackButton/index.jsx
+++ src/components/BackButton/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Container } from "./styles";
 import leftArrow from '../../assets/left_arrw.svg';
 
 export function BackButton() {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1); 
+  };
+
   return (
-    <Container type="button">
+    <Container type="button" onClick={handleGoBack}>
       <img src={leftArrow} alt="Seta para esquerda" />
       voltar
     </Container>

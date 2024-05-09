1924d9071bd385485621bb08fecc3d94efa93c48
Caio Balduino <caiobrafachino@hotmail.com>
2024-05-09 12:12:58 -0300

    feat: desktop create dish

--- src/pages/CreateDish/index.jsx
+++ src/pages/CreateDish/index.jsx
@@ -1,18 +1,129 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { BackButton } from "../../components/BackButton";
 import { Button } from "../../components/Button/Button";
 import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/Footer";
-
 import { Container } from "./styles";
 
 export function CreateDish() {
+  const [dishImage, setDishImage] = useState(null);
+  const [dishName, setDishName] = useState("");
+  const [dishCategory, setDishCategory] = useState("");
+  const [dishTags, setDishTags] = useState("");
+  const [dishPrice, setDishPrice] = useState("");
+  const [dishDescription, setDishDescription] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append("name", dishName);
+      formData.append("description", dishDescription);
+      formData.append("image", dishImage);
+      formData.append("price", dishPrice);
+      formData.append("category", dishCategory);
+      formData.append("ingredients", dishTags);
+
+      const response = await fetch("/api/dishes", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error("Erro ao criar prato");
+      }
+
+      const data = await response.json();
+      const newDishId = data.dish_id;
+      navigate(`/dish/${newDishId}`);
+    } catch (error) {
+      console.error("Erro ao criar prato:", error);
+    }
+  };
 
   return (
     <Container>
-       <Header/>
-
-
-       <Footer/>
+      <Header />
+      <section className="section_one">
+        <BackButton />
+        <h1>Adicionar prato</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form_upload">
+            <span>Imagem do Prato</span>
+            <label htmlFor="dishImage">
+              <img src="/src/assets/upload.svg" alt="" />
+              Selecionar imagem
+            </label>
+            <input
+              type="file"
+              id="dishImage"
+              accept="image/*"
+              onChange={(e) => setDishImage(e.target.files[0])}
+            />
+          </div>
+          <div className="form_name">
+            <label htmlFor="dishName">Nome</label>
+            <input
+              type="text"
+              id="dishName"
+              placeholder="Ex.: Salada Ceasar"
+              value={dishName}
+              onChange={(e) => setDishName(e.target.value)}
+            />
+          </div>
+          <div className="form_category">
+            <label htmlFor="dishCategory">Categoria</label>
+            <select
+              id="dishCategory"
+              value={dishCategory}
+              onChange={(e) => setDishCategory(e.target.value)}
+            >
+              <option value="">Selecione uma categoria</option>
+              <option value="refeição">Refeição</option>
+              <option value="sobremesas">Sobremesas</option>
+              <option value="bebidas">Bebidas</option>
+            </select>
+          </div>
+        </form>
+      </section>
+      <section className="section_two">
+        <form action="">
+          <div className="form_tags">
+            <label htmlFor="dishTags">Ingredientes</label>
+            <input
+              type="text"
+              id="dishTags"
+              value={dishTags}
+              onChange={(e) => setDishTags(e.target.value)}
+            />
+          </div>
+          <div className="form_price">
+            <label htmlFor="dishPrice">Preço</label>
+            <input
+              type="text"
+              id="dishPrice"
+              placeholder="R$ 00,00"
+              value={dishPrice}
+              onChange={(e) => setDishPrice(e.target.value)}
+            />
+          </div>
+        </form>
+      </section>
+      <section className="section_three">
+        <div className="form_description">
+          <label htmlFor="dishDescription">Descrição:</label>
+          <textarea
+            id="dishDescription"
+            value={dishDescription}
+            placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
+            onChange={(e) => setDishDescription(e.target.value)}
+          ></textarea>
+        </div>
+      </section>
+      <Button type="submit">Salvar alterações</Button>
+      <Footer />
     </Container>
   );
-
 }
--- src/pages/CreateDish/styles.js
+++ src/pages/CreateDish/styles.js
@@ -1,5 +1,103 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  
+  height: 100vh;
+  .section_one {
+    padding: 4rem 12.3rem 3.2rem 12.3rem;
+    > h1 {
+      color: ${({ theme }) => theme.COLORS.LIGHT_300};
+      padding: 2.4rem 0rem 3.2rem 0rem;
+      font-size: 3.2rem;
+      font-weight: 400;
+    }
+    > form {
+      display: flex;
+      gap: 3.2rem;
+    }
+  }
+  .form_upload {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    > span {
+      font-family: ${({ theme }) => theme.FONTS.Secondary};
+      color: ${({ theme }) => theme.COLORS.LIGHT_400};
+      font-size: 1.6rem;
+      margin-bottom: 1.6rem;
+    }
+    label {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 4.8rem;
+      width: 100%;
+      background-color: ${({ theme }) => theme.COLORS.DARK_800};
+      font-family: ${({ theme }) => theme.FONTS.Primary};
+      font-size: 1.4rem;
+      border-radius: 0.8rem;
+      position: relative;
+      img {
+        margin-right: 1rem;
+      }
+    }
+    input[type="file"] {
+      display: none;
+    }
+  }
+
+  .form_name,
+  .form_category,
+  .form_tags,
+  .form_price {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    font-family: ${({ theme }) => theme.FONTS.Secondary};
+    color: ${({ theme }) => theme.COLORS.LIGHT_400};
+    > label {
+      font-size: 1.6rem;
+      margin-bottom: 1.6rem;
+    }
+    > input,
+    > select {
+      height: 4.8rem;
+      width: 100%;
+      padding-left: 1.4rem;
+      background-color: ${({ theme }) => theme.COLORS.DARK_800};
+      color: ${({ theme }) => theme.COLORS.LIGHT_400};
+    }
+  }
+
+
+
+  .section_two {
+    padding: 0rem 12.3rem 3.2rem 12.3rem;
+    > form {
+      display: flex;
+      gap: 3.2rem;
+    }
+  }
+
+  .section_three {
+    padding: 0rem 12.3rem 19.6rem 12.3rem;
+  }
+  .form_description {
+    display: flex;
+    flex-direction: column;
+    > label {
+      font-family: ${({ theme }) => theme.FONTS.Secondary};
+      color: ${({ theme }) => theme.COLORS.LIGHT_400};
+      font-size: 1.6rem;
+      margin-bottom: 1.6rem;
+    }
+    > textarea {
+      height: 17.2rem;
+      width: 100%;
+      resize: none;
+      padding: 1.4rem;
+      font-family: ${({ theme }) => theme.FONTS.Secondary};
+      background-color: ${({ theme }) => theme.COLORS.DARK_800};
+      color: ${({ theme }) => theme.COLORS.LIGHT_400};
+    }
+  }
 `;
